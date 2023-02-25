b45f95293b90cadb59e8a461794ceb132a17b1cf
Naninara <nmvmanikanta@gmail.com>
2023-02-25 17:49:17 +0530

    new commit

---
+++ Trees/Text.java
@@ -0,0 +1,9 @@
+package Trees;
+
+public class Text {
+    //1.full binary tree :- every node has 2 or 0 children
+    //2.complete binary tree :- every level should be filled completly filled except last && if last node left filled then its completly binary tree even right not completed
+    //3.prfect binary tree :- in tree all leaf nodes are in same depth and as well as all non leaf nodes has two children
+    //4.balanced binary tree:- the height of tree should be in logn where n is number of nodes
+    //5.degenertive binary trees:- the binary has only left or right travese only
+}
